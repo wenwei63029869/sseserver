24aae35841a753505db05ab81ae64f5a0ba6c055
wenwei63029869 <wwen1030@gmail.com>
2020-07-20 22:09:32 -0700

    expose Hub

--- hub.go
+++ hub.go
@@ -8,24 +8,24 @@ import (
 	"github.com/azer/debug"
 )
 
-// A hub keeps track of all the active client connections, and handles
+// A Hub keeps track of all the active client connections, and handles
 // broadcasting messages out to those connections that match the appropriate
 // namespace.
 //
-// The hub is effectively the "heart" of a Server, but is kept private to hide
+// The Hub is effectively the "heart" of a Server, but is kept private to hide
 // implementation detais.
-type hub struct {
+type Hub struct {
 	broadcast   chan SSEMessage      // Inbound messages to propagate out.
 	connections map[*connection]bool // Registered connections.
 	register    chan *connection     // Register requests from the connections.
 	unregister  chan *connection     // Unregister requests from connections.
 	shutdown    chan bool            // Internal chan to handle shutdown notification
 	sentMsgs    uint64               // Msgs broadcast since startup
-	startupTime time.Time            // Time hub was created
+	startupTime time.Time            // Time Hub was created
 }
 
-func newHub() *hub {
-	return &hub{
+func NewHub() *Hub {
+	return &Hub{
 		broadcast:   make(chan SSEMessage),
 		connections: make(map[*connection]bool),
 		register:    make(chan *connection),
@@ -35,26 +35,26 @@ func newHub() *hub {
 	}
 }
 
-// Shutdown method for cancellation of hub run loop.
+// Shutdown method for cancellation of Hub run loop.
 //
 // right now we are only using this in tests, in the future we may want to be
 // able to more gracefully shut down a Server in production as well, but...
 //
 // TODO: need to do some thinking before exposing this interface more broadly.
-func (h *hub) Shutdown() {
+func (h *Hub) Shutdown() {
 	h.shutdown <- true
 }
 
-// Start begins the main run loop for a hub in a background go func.
-func (h *hub) Start() {
+// Start begins the main run loop for a Hub in a background go func.
+func (h *Hub) Start() {
 	go h.run()
 }
 
-func (h *hub) run() {
+func (h *Hub) run() {
 	for {
 		select {
 		case <-h.shutdown:
-			debug.Debug(fmt.Sprintf("hub shutdown requested, cancelling %d connections...", len(h.connections)))
+			debug.Debug(fmt.Sprintf("Hub shutdown requested, cancelling %d connections...", len(h.connections)))
 			for c := range h.connections {
 				h._shutdownConn(c)
 			}
@@ -73,16 +73,16 @@ func (h *hub) run() {
 	}
 }
 
-// internal method, removes that client from the hub and tells it to shutdown
+// internal method, removes that client from the Hub and tells it to shutdown
 // _unregister is safe to call multiple times with the same connection
-func (h *hub) _unregisterConn(c *connection) {
+func (h *Hub) _unregisterConn(c *connection) {
 	delete(h.connections, c)
 }
 
-// internal method, removes that client from the hub and tells it to shutdown
+// internal method, removes that client from the Hub and tells it to shutdown
 // must only be called once for a given connection to avoid panic!
-func (h *hub) _shutdownConn(c *connection) {
-	// for maximum safety, ALWAYS unregister a connection from the hub prior to
+func (h *Hub) _shutdownConn(c *connection) {
+	// for maximum safety, ALWAYS unregister a connection from the Hub prior to
 	// shutting it down, as we want no possibility of a send on closed channel
 	// panic.
 	h._unregisterConn(c)
@@ -93,7 +93,7 @@ func (h *hub) _shutdownConn(c *connection) {
 
 // internal method, broadcast a message to all matching clients if this fails
 // due to any client having a full send buffer,
-func (h *hub) _broadcastMessage(msg SSEMessage) {
+func (h *Hub) _broadcastMessage(msg SSEMessage) {
 	formattedMsg := msg.sseFormat()
 	for c := range h.connections {
 		if strings.HasPrefix(msg.Namespace, c.namespace) {
